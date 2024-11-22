07d8ab743b412f49c0441c276c88087b5e228249
vascokarla <vascok@michigan.gov>
2024-11-22 15:48:23 -0500

    Updated citations and nextflow command for help menu

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -8,21 +8,21 @@ class WorkflowMain {
     // Citation string for pipeline
     //
     public static String citation(workflow) {
-        return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
+        return "If you use MI-Bioinformatics/process-bact-cluster-per-species for your analysis please cite:\n\n" +
             // TODO nf-core: Add Zenodo DOI for pipeline after first release
             //"* The pipeline\n" +
             //"  https://doi.org/10.5281/zenodo.XXXXXXX\n\n" +
             "* The nf-core framework\n" +
             "  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
             "* Software dependencies\n" +
-            "  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
+            "  https://github.com/MI-Bioinformatics/process-bact-cluster-per-species/blob/master/CITATIONS.md"
     }
 
     //
     // Generate help string
     //
     public static String help(workflow, params, log) {
-        def command = "nextflow run ${workflow.manifest.name} --input samplesheet.csv --genome GRCh37 -profile docker"
+        def command = "nextflow run MI-Bioinformatics/process-bact-cluster-per-species --input manifest.csv --output ./results -profile docker"
         def help_string = ''
         help_string += NfcoreTemplate.logo(workflow, params.monochrome_logs)
         help_string += NfcoreSchema.paramsHelp(workflow, params, command)
@@ -81,7 +81,7 @@ class WorkflowMain {
 
         // Check input has been provided
         if (!params.input) {
-            log.error "Please provide an input samplesheet to the pipeline e.g. '--input samplesheet.csv'"
+            log.error "Please provide an input manifest to the pipeline e.g. '--input manifest.csv'"
             System.exit(1)
         }
     }
