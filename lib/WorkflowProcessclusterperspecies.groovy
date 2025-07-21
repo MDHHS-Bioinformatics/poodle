7bbcc9d8179af430966ba07c10b37d64266dda98
vascokarla <vascok@michigan.gov>
2025-07-21 08:50:39 -0400

    eliminated genome check since it's not a param of the pipeline

--- lib/WorkflowProcessclusterperspecies.groovy
+++ lib/WorkflowProcessclusterperspecies.groovy
@@ -10,8 +10,6 @@ class WorkflowProcessclusterperspecies {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        genomeExistsError(params, log)
-
         if (!params.input) {
             log.error "Input samplesheet not specified with e.g. '--input samplesheet.csv'."
             System.exit(1)
@@ -60,17 +58,5 @@ class WorkflowProcessclusterperspecies {
         def description_html = engine.createTemplate(methods_text).make(meta)
 
         return description_html
-    }//
-    // Exit pipeline if incorrect --genome key provided
-    //
-    private static void genomeExistsError(params, log) {
-        if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
-            log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
-                "  Currently, the available genome keys are:\n" +
-                "  ${params.genomes.keySet().join(", ")}\n" +
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            System.exit(1)
-        }
     }
 }
